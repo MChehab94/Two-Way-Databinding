1cf8b5f3712b7efb15e924f72bbcb463d8e3fd72
Mohamad <lemirmohammad.shehab@lau.edu>
2018-01-05 21:21:51 +0200

    Made the class observable

--- app/src/main/java/mchehab/com/twowaydatabinding/Person.java
+++ app/src/main/java/mchehab/com/twowaydatabinding/Person.java
@@ -8,7 +8,7 @@ import android.databinding.PropertyChangeRegistry;
  * Created by muhammadchehab on 1/5/18.
  */
 
-public class Person{
+public class Person implements Observable{
 
     private PropertyChangeRegistry propertyChangeRegistry = new PropertyChangeRegistry();
 
@@ -27,43 +27,43 @@ public class Person{
         this.age = age;
     }
 
-//    @Bindable
+    @Bindable
     public String getFirstName() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
-//        propertyChangeRegistry.notifyChange(this, BR.firstName);
+        propertyChangeRegistry.notifyChange(this, BR.firstName);
     }
 
-//    @Bindable
+    @Bindable
     public String getLastName() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-//        propertyChangeRegistry.notifyChange(this, BR.lastName);
+        propertyChangeRegistry.notifyChange(this, BR.lastName);
     }
 
-//    @Bindable
+    @Bindable
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
         this.age = age;
-//        propertyChangeRegistry.notifyChange(this, BR.age);
+        propertyChangeRegistry.notifyChange(this, BR.age);
     }
 
-//    @Override
-//    public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
-//        propertyChangeRegistry.add(callback);
-//    }
-//
-//    @Override
-//    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
-//        propertyChangeRegistry.remove(callback);
-//    }
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        propertyChangeRegistry.add(callback);
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        propertyChangeRegistry.remove(callback);
+    }
 }
